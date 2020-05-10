88b610b499db70671310acaa73f64ee194eedc87
kelz <kelsie.langston@yahoo.com>
2020-05-10 12:35:30 -0400

    repoint back to heroku urls

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const { google } = require("googleapis");
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  "http://localhost:8080/member-access"
+  "https:workday-planner.herokuapp.com/member-access"
 );
 
 const url = oauth2Client.generateAuthUrl({
